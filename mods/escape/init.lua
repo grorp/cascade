cc2b33a9eecdca8697161714373011b5e1c83e11
Gregor Parzefall <gregor.parzefall@posteo.de>
2022-01-26 11:11:07 +0100

    Don't spawn players in the air

--- mods/escape/init.lua
+++ mods/escape/init.lua
@@ -1,3 +1,4 @@
 dofile(minetest.get_modpath("escape") .. "/sky.lua")
 dofile(minetest.get_modpath("escape") .. "/nodes.lua")
 dofile(minetest.get_modpath("escape") .. "/maze.lua")
+dofile(minetest.get_modpath("escape") .. "/player.lua")
---
+++ mods/escape/player.lua
@@ -0,0 +1,3 @@
+minetest.register_on_newplayer(function(player)
+    player:set_pos(vector.new(2, 111.5, 2))
+end)
