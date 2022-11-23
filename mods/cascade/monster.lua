9d50aff0b4d519234477f82e5b68368d0a98d577
Gregor Parzefall <gregor.parzefall@posteo.de>
2022-11-23 21:32:04 +0100

    Let monsters decelerate if they don't have a target

--- mods/cascade/monster.lua
+++ mods/cascade/monster.lua
@@ -22,18 +22,23 @@ function Monster:on_activate()
     })
 end
 
-local gravity = vector.new(0, -9.81, 0)
+local GRAVITY = vector.new(0, -9.81, 0)
+local MONSTER_RADIUS = 50
+local MONSTER_ACCEL = 5
+local MONSTER_DECEL = 5
 
 function Monster:on_step(dtime, moveresult)
-    self.object:add_velocity(gravity * dtime)
+    self.object:add_velocity(GRAVITY * dtime)
 
     local target
 
-    local candidates = minetest.get_objects_inside_radius(self.object:get_pos(), 50)
+    local candidates = minetest.get_objects_inside_radius(
+        self.object:get_pos(), MONSTER_RADIUS
+    )
     for _, candidate in ipairs(candidates) do
         if candidate:is_player() and (not target or
-            vector.distance(self.object.get_pos(), candidate.get_pos()) <
-            vector.distance(self.object.get_pos(), target.get_pos())
+            vector.distance(self.object:get_pos(), candidate:get_pos()) <
+            vector.distance(self.object:get_pos(), target:get_pos())
         ) then
             target = candidate
         end
@@ -43,13 +48,24 @@ function Monster:on_step(dtime, moveresult)
         local attack_dir = vector.direction(self.object:get_pos(), target:get_pos())
         attack_dir.y = 0
         attack_dir = attack_dir:normalize()
-        self.object:add_velocity(attack_dir * 5 * dtime)
+        self.object:add_velocity(attack_dir * MONSTER_ACCEL * dtime)
+    else
+        local vel = self.object:get_velocity()
+        local decel_dir = vector.new(-vel.x, 0, -vel.z):normalize()
+        local decel = decel_dir * MONSTER_DECEL * dtime
+        if math.abs(decel.x) > math.abs(vel.x) then
+            decel.x = -vel.x
+        end
+        if math.abs(decel.z) > math.abs(vel.z) then
+            decel.z = -vel.z
+        end
+        self.object:set_velocity(vel + decel)
+    end
 
-        for _, collision in ipairs(moveresult.collisions) do
-            if collision.type == "object" and collision.object == target then
-                shared.fail(target)
-                break
-            end
+    for _, collision in ipairs(moveresult.collisions) do
+        if collision.type == "object" and collision.object:is_player() then
+            shared.fail(target)
+            break
         end
     end
 end
