79e1aea98faf3c3bcb2f43fa05c24072a47bca15
Gregor Parzefall <gregor.parzefall@posteo.de>
2022-11-25 18:11:40 +0100

    Add sprinting like in MC

--- mods/cascade/player.lua
+++ mods/cascade/player.lua
@@ -1,3 +1,13 @@
+-- Sprinting in Minecraft:
+-- https://minecraft.fandom.com/wiki/Sprinting?oldid=2190082
+-- Sprinting in MineClone 2:
+-- https://git.minetest.land/MineClone2/MineClone2/src/commit/0942949c5da465c7886aa44cd0267d535658e9ea/mods/PLAYER/mcl_sprint
+
+local PLAYER_WALK_SPEED = 1.125 -- 4.5 m/s
+local PLAYER_RUN_SPEED = PLAYER_WALK_SPEED * 1.3 -- 5.85 m/s
+local PLAYER_RUN_FOV = 1.15
+local PLAYER_FOV_TRANSITION_DURATION = 0.15
+
 minetest.register_on_joinplayer(function(player)
     player:set_properties({
         visual = "cube",
@@ -8,20 +18,17 @@ minetest.register_on_joinplayer(function(player)
             "cascade_player_front.png", "cascade_player_side.png",
         },
         pointable = false,
-
         physical = true,
-        collisionbox = {
-            -7/16, -15/16, -7/16,
-             7/16,  15/16,  7/16,
-        },
-
-        -- https://github.com/minetest/minetest/blob/5.6.1/doc/lua_api.txt#L7643
-        nametag_color = "#00000000",
+        collisionbox = {-7/16, -15/16, -7/16, 7/16, 15/16, 7/16},
 
         eye_height = 9/16 + 1/32, -- The exact eye height of the texture.
+        -- https://github.com/minetest/minetest/blob/5.6.1/doc/lua_api.txt#L7643
+        nametag_color = "#00000000",
     })
 
     player:set_armor_groups({immortal = 1})
+
+    player:set_physics_override({ speed = PLAYER_WALK_SPEED })
 
     player:hud_set_flags({
         basic_debug = false,
@@ -45,4 +52,27 @@ minetest.register_on_chat_message(function()
 end)
 minetest.register_on_chatcommand(function()
     return true
+end)
+
+local map_name→running = {}
+
+minetest.register_globalstep(function()
+    for _, player in ipairs(minetest.get_connected_players()) do
+        local name = player:get_player_name()
+        local ctrl = player:get_player_control()
+
+        if ctrl.aux1 and not map_name→running[name] then
+            player:set_physics_override({ speed = PLAYER_RUN_SPEED })
+            player:set_fov(PLAYER_RUN_FOV, true, PLAYER_FOV_TRANSITION_DURATION)
+            map_name→running[name] = true
+        elseif not ctrl.aux1 and map_name→running[name] then
+            player:set_physics_override({ speed = PLAYER_WALK_SPEED })
+            player:set_fov(0, true, PLAYER_FOV_TRANSITION_DURATION)
+            map_name→running[name] = false
+        end
+    end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+    map_name→running[player:get_player_name()] = nil
 end)
