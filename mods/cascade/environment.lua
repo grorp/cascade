17fc66faed41af7c3afbcbb3fa6513ebf19a00db
Gregor Parzefall <gregor.parzefall@posteo.de>
2023-07-26 14:10:54 +0200

    Support dynamic shadows

--- mods/cascade/environment.lua
+++ mods/cascade/environment.lua
@@ -1,6 +1,26 @@
-minetest.register_on_joinplayer(function(player)
-    player:override_day_night_ratio(1)
+-- `cascade_old_time_speed` might still be set if there has been a crash.
+if not minetest.settings:get("cascade_old_time_speed") then
+    minetest.settings:set("cascade_old_time_speed", minetest.settings:get("time_speed"))
+end
+minetest.settings:set("time_speed", 0)
 
+minetest.register_on_shutdown(function()
+    minetest.settings:set("time_speed", minetest.settings:get("cascade_old_time_speed"))
+    minetest.settings:remove("cascade_old_time_speed")
+end)
+
+local FIXED_TIMEOFDAY = 0.4
+
+local function reset_timeofday()
+    if minetest.get_timeofday() ~= FIXED_TIMEOFDAY then
+        minetest.set_timeofday(FIXED_TIMEOFDAY)
+    end
+    minetest.after(5, reset_timeofday)
+end
+
+reset_timeofday()
+
+minetest.register_on_joinplayer(function(player)
     player:set_sky({
         type = "plain",
         base_color = "#cd6093",
@@ -16,7 +36,10 @@ minetest.register_on_joinplayer(function(player)
     })
 
     player:set_sun({
-        visible = false,
+        visible = true,
+        texture = "", -- ignore "sun.png" provided by texture packs
+        tonemap = "", -- whatever
+        sunrise = "", -- whatever
         sunrise_visible = false,
     })
 
@@ -27,4 +50,14 @@ minetest.register_on_joinplayer(function(player)
     player:set_stars({
         visible = false,
     })
+
+    player:set_lighting({
+        shadows = {
+            intensity = 0.4,
+        },
+    })
+
+    -- For whatever reason, this prevents a delay before the sun appears in
+    -- the right place.
+    minetest.set_timeofday(FIXED_TIMEOFDAY)
 end)
